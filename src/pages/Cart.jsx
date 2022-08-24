a6848c945022c5b39a00b3dc2154f16e9cd44596
joshua Oyewole <joshpp2013@gmail.com>
2022-08-24 21:27:06 +0100

    resolved

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,6 +7,7 @@ import StripeCheckout from "react-stripe-checkout";
 import { useEffect, useState } from "react";
 import { userRequest } from "../requestMethods";
 import { useHistory} from "react-router";
+import { Link } from 'react-router-dom';
 
 const KEY = process.env.REACT_APP_STRIPE;
 
